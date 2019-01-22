d99613e185b83be92418cd6343cb3943ecb6342f
saifembrace <sar@embrace-it.com>
2019-01-22 20:03:30 +0500

    update block name and remove hardcoded version

--- view/frontend/web/js/view/payment/terminal.js
+++ view/frontend/web/js/view/payment/terminal.js
@@ -1,5 +1,5 @@
 /**
- * Altapay Module version 3.0.1 for Magento 2.x.
+ * Altapay Module for Magento 2.x.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
