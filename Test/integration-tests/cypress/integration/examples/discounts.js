0b3fc25a79f2d0d2ce9568d631918fe865da3d46
Muhammad Idrees <79894725+saadidrees711@users.noreply.github.com>
2021-10-25 17:33:15 +0500

    Update discounts.js

--- Test/integration-tests/cypress/integration/examples/discounts.js
+++ Test/integration-tests/cypress/integration/examples/discounts.js
@@ -2,74 +2,74 @@ import Order from '../PageObjects/objects'
 
 describe('Discounts', function () {
 
-    it('Create cart percentage discount', function () {
+//     it('Create cart percentage discount', function () {
 
-        const ord = new Order()
-        ord.clrcookies()
-        ord.admin()
-        ord.create_cart_percent_discount()
+//         const ord = new Order()
+//         ord.clrcookies()
+//         ord.admin()
+//         ord.create_cart_percent_discount()
 
-    })
+//     })
 
-    it('Apply cart percentage discount with CC', function () {
+//     it('Apply cart percentage discount with CC', function () {
 
-        const ord = new Order()
-        ord.clrcookies()
-        ord.visit()
-        ord.apply_cart_percent_discount()
-        ord.complete_checkout()
-        cy.fixture('config').then((admin) => {
-            if (admin.CC_TERMINAL_NAME != "") {
-                cy.get('body').wait(3000).then(($a) => {
-                    if ($a.find("label:contains('" + admin.CC_TERMINAL_NAME + "')").length) {
-                        ord.cc_payment(admin.CC_TERMINAL_NAME)
-                        ord.admin()
-                        ord.capture()
-                        ord.refund()
-                    } else {
-                        cy.log(admin.CC_TERMINAL_NAME + ' not found in page')
-                        this.skip()
-                    }
+//         const ord = new Order()
+//         ord.clrcookies()
+//         ord.visit()
+//         ord.apply_cart_percent_discount()
+//         ord.complete_checkout()
+//         cy.fixture('config').then((admin) => {
+//             if (admin.CC_TERMINAL_NAME != "") {
+//                 cy.get('body').wait(3000).then(($a) => {
+//                     if ($a.find("label:contains('" + admin.CC_TERMINAL_NAME + "')").length) {
+//                         ord.cc_payment(admin.CC_TERMINAL_NAME)
+//                         ord.admin()
+//                         ord.capture()
+//                         ord.refund()
+//                     } else {
+//                         cy.log(admin.CC_TERMINAL_NAME + ' not found in page')
+//                         this.skip()
+//                     }
 
-                })
+//                 })
 
-            }
-            else {
-                cy.log('CC_TERMINAL_NAME skipped')
-                this.skip()
-            }
-        })
-    })
+//             }
+//             else {
+//                 cy.log('CC_TERMINAL_NAME skipped')
+//                 this.skip()
+//             }
+//         })
+//     })
 
-    it('Apply cart percentage discount with Klarna', function () {
+//     it('Apply cart percentage discount with Klarna', function () {
 
-        const ord = new Order()
-        ord.clrcookies()
-        ord.visit()
-        ord.apply_cart_percent_discount()
-        ord.complete_checkout()
-        cy.fixture('config').then((admin) => {
-            if (admin.CC_TERMINAL_NAME != "") {
-                cy.get('body').wait(3000).then(($a) => {
-                    if ($a.find("label:contains('" + admin.KLARNA_DKK_TERMINAL_NAME + "')").length) {
-                        ord.klarna_payment(admin.KLARNA_DKK_TERMINAL_NAME)
-                        ord.admin()
-                        ord.capture()
-                        ord.refund()
-                    } else {
-                        cy.log(admin.CC_TERMINAL_NAME + ' not found in page')
-                        this.skip()
-                    }
+//         const ord = new Order()
+//         ord.clrcookies()
+//         ord.visit()
+//         ord.apply_cart_percent_discount()
+//         ord.complete_checkout()
+//         cy.fixture('config').then((admin) => {
+//             if (admin.CC_TERMINAL_NAME != "") {
+//                 cy.get('body').wait(3000).then(($a) => {
+//                     if ($a.find("label:contains('" + admin.KLARNA_DKK_TERMINAL_NAME + "')").length) {
+//                         ord.klarna_payment(admin.KLARNA_DKK_TERMINAL_NAME)
+//                         ord.admin()
+//                         ord.capture()
+//                         ord.refund()
+//                     } else {
+//                         cy.log(admin.CC_TERMINAL_NAME + ' not found in page')
+//                         this.skip()
+//                     }
 
-                })
+//                 })
 
-            }
-            else {
-                cy.log('CC_TERMINAL_NAME skipped')
-                this.skip()
-            }
-        })
-    })
+//             }
+//             else {
+//                 cy.log('CC_TERMINAL_NAME skipped')
+//                 this.skip()
+//             }
+//         })
+//     })
 
     it('Create cart fixed discount', function () {
 
