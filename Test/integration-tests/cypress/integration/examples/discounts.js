5ce46410bd5f2f7baecc5b4f62b7af2cdaefc810
Muhammad Idrees <79894725+saadidrees711@users.noreply.github.com>
2022-01-12 14:55:38 +0500

    Discount cases run check

--- Test/integration-tests/cypress/integration/examples/discounts.js
+++ Test/integration-tests/cypress/integration/examples/discounts.js
@@ -1,5 +1,5 @@
 import Order from '../PageObjects/objects'
-
+if(Cypress.env('runDiscountsTests')){
 describe('Discounts', function () {
 
     it('Create cart percentage discount', function () {
@@ -416,3 +416,4 @@ describe('Discounts', function () {
     })
 
 })
+}
