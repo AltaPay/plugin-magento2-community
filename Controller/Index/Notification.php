ef056e7fbe749c2b1e8d17090b7fc70a06e2df70
BushraAsif <bushraasif931@gmail.com>
2022-08-17 11:18:32 +0500

    Fix typo

--- Controller/Index/Notification.php
+++ Controller/Index/Notification.php
@@ -65,7 +65,7 @@ class Notification extends Index implements CsrfAwareActionInterface
                     }
                 }
 
-                if (isset($post['error_message']) && $aMessageMustBeShown == "true") {
+                if (isset($post['error_message']) && $cardHolderMessageMustBeShown == "true") {
                     $msg = $post['error_message'];
                 } else {
                     $msg = "Error with the Payment.";
