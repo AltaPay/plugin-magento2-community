3a8056587de2bf3d84e845019a838bb694acd472
Bushra Asif <bushraasif931@gmail.com>
2022-11-15 12:52:24 +0500

    Show enabled terminal on frontend checkout page

---
+++ view/frontend/web/js/view/payment/method-renderer/terminal8-method.js
@@ -0,0 +1,24 @@
+/**
+ * Altapay Module for Magento 2.x.
+ *
+ * Copyright © 2018 Altapay. All rights reserved.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*browser:true*/
+/*global define*/
+
+define(
+    [
+        'SDM_Altapay/js/view/payment/method-renderer/terminal-abstract'
+    ],
+    function (Component) {
+        'use strict';
+        return Component.extend({
+            defaults: {
+                terminal: '8'
+            }
+        });
+    }
+);
