ec6b2bbacef8dc3ae1d191b0181fe2d4c62eca28
Bushra Asif <bushraa@altapay.com>
2025-05-21 10:47:19 +0500

    Properly deactivate the quote when order is successful

--- Model/Generator.php
+++ Model/Generator.php
@@ -631,6 +631,23 @@ class Generator
                 if (strtolower($paymentType) === 'paymentandcapture' || strtolower($paymentType) === 'subscriptionandcharge') {
                     $this->paymentHandler->createInvoice($order);
                 }
+
+                // Properly deactivate the quote when order is successful
+                if ($status === 'succeeded' || $status === 'success') {
+                    $this->altapayLogger->addDebugLog('Deactivating quote for successful order', $order->getIncrementId());
+                    try {
+                        $quote = $this->quote->loadByIdWithoutStore($order->getQuoteId());
+                        if ($quote->getId()) {
+                            $quote->setIsActive(false)->setReservedOrderId(null)->save();
+                            
+                            // Clear the checkout session quote
+                            $this->checkoutSession->clearQuote();
+                            $this->checkoutSession->clearStorage();
+                        }
+                    } catch (\Exception $e) {
+                        $this->altapayLogger->addCriticalLog('Exception while deactivating quote', $e->getMessage());
+                    }
+                }
             }
         }
     }
