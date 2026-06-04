2e974ee64106332dec7f89cd93629b0145d80f7b
Bushra Asif <bushraasif931@gmail.com>
2026-06-04 16:25:24 +0500

    Update checkout session ID handling to include order increment ID

--- Model/Gateway.php
+++ Model/Gateway.php
@@ -519,9 +519,9 @@ class Gateway implements GatewayInterface
             $storeCode       = $order->getStore()->getCode();
             $activeTerminals = $this->getActiveTerminals($request, $storeScope, $storeCode);
 
-            $sessionId = $this->checkoutSession->getData('altapay_checkout_session_id');
+            $sessionId = $this->checkoutSession->getData('altapay_checkout_session_id_' . $order->getIncrementId());
 
-            if ($sessionId !== 'session-' . $order->getQuoteId() . '-' . $order->getIncrementId()) {
+            if (empty($sessionId)) {
                 try {
                     $sessionId = 'session-' . $order->getQuoteId() . '-' . $order->getIncrementId();
                     $marketPaySession = new CheckoutSession($this->systemConfig->getAuth($storeCode));
@@ -536,7 +536,7 @@ class Gateway implements GatewayInterface
                     if (isset($checkoutResponse->Session->Id)) {
                         $sessionId = $checkoutResponse->Session->Id;
                     }
-                    $this->checkoutSession->setData('altapay_checkout_session_id', $sessionId);
+                    $this->checkoutSession->setData('altapay_checkout_session_id_' . $order->getIncrementId(), $sessionId);
                 } catch (\Exception $e) {
                     $this->altapayLogger->addCriticalLog('CheckoutSession Exception', $e->getMessage());
                 }
